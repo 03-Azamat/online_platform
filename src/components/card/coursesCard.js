e7009432e083dfdda082b3afe450c41a228df4e8
03-Azamat <azamatnurov10@gmail.com>
2022-05-08 10:07:41 +0600

    for

--- src/components/card/coursesCard.js
+++ src/components/card/coursesCard.js
@@ -6,16 +6,18 @@ const CoursesCard = ({el}) => {
     const calendarDate = format(new Date(), "dd")
 
     return (
+        <NavLink to={`/coursesDetails/${el.id}`}>
             <div className="coursesCard" key={el.id}>
-                <NavLink to={`/coursesDetails/${el.id}`}>
+
                     <img src={el.img} alt="" className="rounded-t-md w-full max-h-60"/>
-                </NavLink>
                 <div className="coursesCard--box">
                     <h1 className="coursesCard--box--title">{el.title}</h1>
                     <p className="coursesCard--box--date">{calendarDate} неделя</p>
                     <button className="coursesCard--box--price">{el.price}</button>
                 </div>
             </div>
+        </NavLink>
+
     );
 };
 
